9bd3ae60b5a92542522ae784ab546701bba8f841
krdecker <krd.345@gmail.com>
2015-12-12 14:49:12 -0800

    move model to Cat function

--- js/app.js
+++ js/app.js
@@ -1,14 +1,9 @@
-var ViewModel = function () {
+var Cat = function () {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('');
     this.nickNames = ko.observableArray(['Bozo','Dozo','Fozo','Gozo']);
-
-    this.incrementCounter = function () {
-        this.clickCount(this.clickCount() + 1);
-    }
-
     this.level = [
             'Newborn',
             'Kitty',
@@ -16,17 +11,18 @@ var ViewModel = function () {
             'Rogue',
             'FatCat'
         ];
-
-
-
     this.catLevel = ko.pureComputed( function () {
         var age = 0;
         age = Math.floor(this.clickCount() / 16);
-        //console.log(age);
         if (age >= this.level.length) age = this.level.length - 1;
         return this.level[age];
     }, this);
+}
 
+var ViewModel = function () {
+    this.incrementCounter = function () {
+        this.clickCount(this.clickCount() + 1);
+    }
 }
 
 ko.applyBindings(new ViewModel())
