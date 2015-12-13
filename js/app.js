efd0e6ca128c38ced95c274a0fb59a94eced9898
krdecker <krd.345@gmail.com>
2015-12-13 07:00:29 -0800

    create catList

--- js/app.js
+++ js/app.js
@@ -1,5 +1,49 @@
+
+var initialCats = [
+        {
+            clickCount : 0,
+            name : 'Tabby',
+            imgSrc : 'img/434164568_fea0ad4013_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568',
+            nickNames: ['Bozo','Dozo','Fozo','Gozo'],
+            levels: ['Newborn','Kitty','Mischief','Rogue','FatCat']
+        },
+        {
+            clickCount : 0,
+            name : 'Tiger',
+            imgSrc : 'img/4154543904_6e2428c421_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904',
+            nickNames: ['Bozer','Dozer','Fozer','Gozer'],
+            levels: ['Newb','Kit','Miscreant','Pirate','Retiree']
+        },
+        {
+            clickCount : 0,
+            name : 'Scaredy',
+            imgSrc : 'img/22252709_010df3379e_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709',
+            nickNames: ['Beezer','Deezer','Feezer','Geezer'],
+            levels: ['Cutey','Putey','Scutey','Rutey','Futey']
+        },
+        {
+            clickCount : 0,
+            name : 'Shadow',
+            imgSrc : 'img/1413379559_412a540d29_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559',
+            nickNames: ['Bozo','Dozo','Fozo','Gozo'],
+            levels: ['Newborn','Kitty','Mischief','Rogue','FatCat']
+        },
+        {
+            clickCount : 0,
+            name : 'Sleepy',
+            imgSrc : 'img/9648464288_2516b35537_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288',
+            nickNames: ['Bozo','Dozo','Fozo','Gozo'],
+            levels: ['Newborn','Kitty','Mischief','Rogue','FatCat']
+        }
+    ];
+
 var Cat = function (data) { // data will be an (array of) object literal(s)
-    this.clickCount = ko.observable(data.clicks);
+    this.clickCount = ko.observable(data.clickCount);
     this.name = ko.observable(data.name);
     this.imgSrc = ko.observable(data.imgSrc);
     this.imgAttribution = ko.observable(data.imgAttrib);
@@ -15,23 +59,19 @@ var Cat = function (data) { // data will be an (array of) object literal(s)
 
 var ViewModel = function () {
     var self = this;
+    this.catList = ko.observableArray([]);
+    initialCats.forEach(function(catItem) {
+        self.catList.push( new Cat(catItem));
+    });
 
-    this.currentCat = ko.observable( new Cat(
-            {
-                clicks: 0,
-                name: 'Tabby',
-                imgSrc: 'img/434164568_fea0ad4013_z.jpg',
-                imgAttrib: '',
-                nickNames: ['Bozo','Dozo','Fozo','Gozo'],
-                levels: ['Newborn','Kitty','Mischief','Rogue','FatCat']
-            }
-        ) );
-
+    this.currentCat = ko.observable( this.catList()[0]);
+    console.log(this.currentCat().clickCount());
     this.incrementCounter = function () { // when this func is called on click binding
                                         // context is with:currentCat ie = this
                                         // so need to use self to pass ViewModel
                                         // OR ELSE:
                                         // this.clickCount(this.clickCount()+1);
+        console.log(self.currentCat().clickCount());
         self.currentCat().clickCount(self.currentCat().clickCount() + 1);
     }
     // this.incrementCounter = function () {
