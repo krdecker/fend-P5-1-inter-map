71d7ac5ebeba8f578959b2eb42c7f5dbff954062
krdecker <krd.345@gmail.com>
2015-12-28 22:56:12 -0800

    include ajax jquery examples from iss api

--- src/js/app.js
+++ src/js/app.js
@@ -111,3 +111,21 @@ var ViewModel = function () {
 };
 
 ko.applyBindings(new ViewModel())
+
+// from ISS API website: (maybe useful for translink api)
+
+// $.getJSON('http://api.open-notify.org/iss-pass.json?lat=45.0&lon=-122.3&alt=20&n=5&callback=?', function(data) {
+//     data['response'].forEach(function (d) {
+//         var date = new Date(d['risetime']*1000);
+//          $('#isspass').append('<li>' + date.toString() + '</li>');
+//     });
+// });
+
+// $.getJSON('http://api.open-notify.org/astros.json?callback=?', function(data) {
+//     var number = data['number'];
+//     $('#spacepeeps').html(number);
+
+//     data['people'].forEach(function (d) {
+//          $('#astronames').append('<li>' + d['name'] + '</li>');
+//     });
+// });
