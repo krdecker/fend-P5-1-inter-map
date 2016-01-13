6da80bf333893f384e9daed8ad60227e3b5f0ba2
krdecker <krd.345@gmail.com>
2016-01-12 16:54:57 -0800

    rough data model and bare map styling

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,8 @@
 
     // **Broadway-Commercial Station**
 
+// user chooses a map-view:
+
     // TRAINS - filter by line
 
     // BUSES - filter by direction EB, WB, NB, SB; filter by route no.
@@ -13,67 +15,79 @@
 
     // TO-DO - filter by adult, children])
 
+///////////////////MODEL///////////////////////
 
-// cat data:
-var initialCats = [
+//get & hold various chunks of data
+// json objects returned from apis
+// lists of sites on each map-view
+
+// trains data: ???
+var trainsModel = [
+        {},
+        {}
+    ];
+
+// bus data:
+var busModel = [];
+
+// eats data: an array of places
+var eatsModel = [
         {
-            clickCount : 0,
-            name : 'Tabby',
-            imgSrc : 'img/434164568_fea0ad4013_z.jpg',
-            imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568',
-            nickNames: ['Bozo','Dozo','Fozo','Gozo'],
-            levels: ['Newborn','Kitty','Mischief','Rogue','FatCat']
+            name : 'A&W Restaurant : Burgers Fast',
+            location : {lat: 49.263, lng: -123.0695}
         },
         {
-            clickCount : 0,
-            name : 'Tiger',
-            imgSrc : 'img/4154543904_6e2428c421_z.jpg',
-            imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904',
-            nickNames: ['Bozer','Dozer','Fozer','Gozer'],
-            levels: ['Newb','Kit','Miscreant','Pirate','Retiree']
-        },
-        {
-            clickCount : 0,
-            name : 'Scaredy',
-            imgSrc : 'img/22252709_010df3379e_z.jpg',
-            imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709',
-            nickNames: ['Beezer','Deezer','Feezer','Geezer'],
-            levels: ['Cutey','Putey','Scutey','Rutey','Futey']
-        },
-        {
-            clickCount : 0,
-            name : 'Shadow',
-            imgSrc : 'img/1413379559_412a540d29_z.jpg',
-            imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559',
-            nickNames: ['Bozo','Dozo','Fozo','Gozo'],
-            levels: ['Newborn','Kitty','Mischief','Rogue','FatCat']
-        },
-        {
-            clickCount : 0,
-            name : 'Sleepy',
-            imgSrc : 'img/9648464288_2516b35537_z.jpg',
-            imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288',
-            nickNames: ['Bozo','Dozo','Fozo','Gozo'],
-            levels: ['Newborn','Kitty','Mischief','Rogue','FatCat']
+            name : '',
+            location : {lat: 49.263, lng: -123.0695}
         }
     ];
 
-var Cat = function (data) { // data will be an (array of) object literal(s)
-    this.clickCount = ko.observable(data.clickCount);
-    this.name = ko.observable(data.name);
-    this.imgSrc = ko.observable(data.imgSrc);
-    this.imgAttribution = ko.observable(data.imgAttrib);
-    this.nickNames = ko.observableArray(data.nickNames);
-    this.levels = data.levels;
-    this.catLevel = ko.computed( function () {
-        var age = 0;
-        age = Math.floor(this.clickCount() / 16);
-        if (age >= this.levels.length) age = this.levels.length - 1;
-        return this.levels[age];
-    }, this);
-}
+// shops data:
+var shopsModel = [
+        {
+            name : 'Pharmasave',
+            location : {lat: 49.263, lng: -123.0695}
+        },
+        {
+            name : '',
+            location : {lat: 49.263, lng: -123.0695}
+        }
+    ];
+
+///////////////MAP API///////////////////////////
 
 var map;
+
+//get rid of all Google's POI and Transit features on our map
+var bareStyles = [
+        {
+            featureType: "poi",
+            elementType: "labels",
+            stylers: [
+                  { visibility: "off" }
+            ]
+        },
+        {
+            featureType: "transit",
+            elementType: "labels",
+            stylers: [
+                  { visibility: "off" }
+            ]
+        }
+      ];
+
+var mapOptions = {
+        center: {lat: 49.263, lng: -123.0695},
+        // Commercial Broadway Station 49°15′45″N, 123°04′08″W
+        scrollwheel: false,
+        zoom: 19, // tighter for transit
+        //zoom: 18 // for eats etc
+        zoomControl: false,
+        scrollwheel: false,
+        draggable: false,
+        disableDefaultUI: true,
+        styles: bareStyles
+    }
 
 // function initMap() {
 //     var mapOptions = {
@@ -87,54 +101,73 @@ var map;
 
 function initMap() {
     // Create a map object and specify the DOM element for display.
-    map = new google.maps.Map(document.getElementById('map-div'), {
-        center: {lat: 49.263, lng: -123.0695},
-        // Commercial Broadway Station 49°15′45″N, 123°04′08″W
-        scrollwheel: false,
-        zoom: 19 // tighter for transit
-        //zoom: 18 // for eats etc
-    });
+    map = new google.maps.Map(document.getElementById('map-div'), mapOptions );
 }
+
+
+/////////////////KO VIEWMODEL///////////////////
+
+// this is a Cat class: he made a viewmodel object for each cat
+// var Cat = function (data) { // data will be an (array of) object literal(s)
+//     this.clickCount = ko.observable(data.clickCount);
+//     this.name = ko.observable(data.name);
+//     this.imgSrc = ko.observable(data.imgSrc);
+//     this.imgAttribution = ko.observable(data.imgAttrib);
+//     this.nickNames = ko.observableArray(data.nickNames);
+//     this.levels = data.levels;
+//     this.catLevel = ko.computed( function () {
+//         var age = 0;
+//         age = Math.floor(this.clickCount() / 16);
+//         if (age >= this.levels.length) age = this.levels.length - 1;
+//         return this.levels[age];
+//     }, this);
+// }
+
+// ???? so maybe a MapView class, instances for  trainVM, busVM, eatsVM etc
 
 var ViewModel = function () {
     var self = this;
 
-    this.station = ko.observable("Broadway-Commercial");
+    // can data bind on 'constants'; don't need the overhead of an .observable
+    this.station = "Broadway-Commercial"; //ko.observable("Broadway-Commercial");
 
-    this.optList = ko.observableArray(["Trains","Buses","Eats","Shops","To Do"]);
+    this.optList = ["Trains","Buses","Eats","Shops","To Do"]; //ko.observableArray(["Trains","Buses","Eats","Shops","To Do"]);
 
     this.mapDisplay = ko.observable("display:none");
+    this.menuDisplay = ko.observable("display:block");
 
 
-    this.catList = ko.observableArray([]);
-    initialCats.forEach( function(catItem) {
-        self.catList.push( new Cat(catItem) );
-    });
+    // this.catList = ko.observableArray([]);
+    // initialCats.forEach( function(catItem) {
+    //     self.catList.push( new Cat(catItem) );
+    // });
 
-    this.currentOpt = ko.observable( this.optList()[0] );
+    this.currentOpt = ko.observable( this.optList[0] );
 
-    this.incrementCounter = function () { // when this func is called on click binding
-                                        // context is with:currentCat ie = this
-                                        // so need to use self to pass ViewModel
-                                        // OR ELSE:
-                                        // this.clickCount(this.clickCount()+1);
+    // this.incrementCounter = function () { // when this func is called on click binding
+    //                                     // context is with:currentCat ie = this
+    //                                     // so need to use self to pass ViewModel
+    //                                     // OR ELSE:
+    //                                     // this.clickCount(this.clickCount()+1);
 
-        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
-    };
+    //     self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+    // };
     // this.incrementCounter = function () {
     //     this.clickCount(this.clickCount() + 1);
     // }
-    this.changeCat = function (clickedCat) { self.currentCat(clickedCat) };
+    // this.changeCat = function (clickedCat) { self.currentCat(clickedCat) };
 
     this.changeOpt = function (clickedOpt) {
         self.currentOpt(clickedOpt);
         console.log("Option= " + self.currentOpt());
+        self.menuDisplay("display:none");
         self.mapDisplay("display:block");
         initMap();
     };
 
     this.menuReturn = function () {
         self.mapDisplay("display:none");
+        self.menuDisplay("display:block");
     }
 };
 
